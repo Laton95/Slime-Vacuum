b84ba37f9094ef4b64927d8744e8f9399dd96743
Laton95 <laton95@gmail.com>
2019-07-31 17:28:02 +1000

    Added support for magma cubes and modded slimes

--- src/main/java/com/laton95/slimevacuum/VacuumItem.java
+++ src/main/java/com/laton95/slimevacuum/VacuumItem.java
@@ -1,5 +1,6 @@
 package com.laton95.slimevacuum;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.monster.SlimeEntity;
@@ -10,8 +11,15 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.storage.loot.LootParameterSets;
+import net.minecraft.world.storage.loot.LootParameters;
+import net.minecraft.world.storage.loot.LootTable;
 
 public class VacuumItem extends Item {
 	
@@ -22,6 +30,7 @@ public class VacuumItem extends Item {
 	@Override
 	public boolean itemInteractionForEntity(ItemStack stack, PlayerEntity player, LivingEntity entity, Hand hand) {
 		if(!entity.world.isRemote() && entity instanceof SlimeEntity) {
+			//Shink slime
 			int size = ((SlimeEntity) entity).getSlimeSize();
 			
 			if(size > 1) {
@@ -33,10 +42,45 @@ public class VacuumItem extends Item {
 				entity.remove();
 			}
 			
+			//Get slimeball item
+			Item slimeItem = Items.AIR;
+			
+			if(entity.getType() == EntityType.SLIME) {
+				slimeItem = Items.SLIME_BALL;
+			}
+			else if(entity.getType() == EntityType.MAGMA_CUBE) {
+				slimeItem = Items.MAGMA_CREAM;
+			}
+			else {
+				//In the case of a slime added by a mod, try and get *something* from the loot table
+				try {
+					LootTable loottable = entity.world.getServer().getLootTableManager().getLootTableFromLocation(entity.getType().getLootTable());
+					LootContext.Builder builder = new LootContext.Builder((ServerWorld)entity.world).withRandom(entity.world.rand).withParameter(LootParameters.THIS_ENTITY, entity).withParameter(LootParameters.POSITION, new BlockPos(entity)).withParameter(LootParameters.DAMAGE_SOURCE, new DamageSource("")).withLuck(1000);
+					
+					int tries = 0;
+					while(slimeItem == Items.AIR) {
+						if(tries > 10) {
+							slimeItem = Items.SLIME_BALL;
+							break;
+						}
+						
+						slimeItem = loottable.generate(builder.build(LootParameterSets.ENTITY)).get(0).getItem();
+						
+						tries++;
+					}
+				}
+				catch(Exception e) {
+					slimeItem = Items.SLIME_BALL;
+					SlimeVacuum.LOGGER.error("Error encountered sucking modded slime, please report to https://github.com/Laton95/Slime-Vacuum/issues");
+				}
+			}
+			
+			//Do misc effects
 			stack.attemptDamageItem(1, player.world.rand, (ServerPlayerEntity) player);
 			entity.playSound(SoundEvents.ENTITY_SLIME_ATTACK, 1.0F, (entity.world.rand.nextFloat() - entity.world.rand.nextFloat()) * 0.2F + 1.0F);
 			
-			ItemStack slimeBalls = new ItemStack(Items.SLIME_BALL, size);
+			//Give slimeball
+			ItemStack slimeBalls = new ItemStack(slimeItem, size);
 			ItemEntity slimeBallsEntity = new ItemEntity(player.world, player.posX, player.posY, player.posZ, slimeBalls);
 			player.world.addEntity(slimeBallsEntity);
 			
